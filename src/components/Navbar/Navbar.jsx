51c7a0a89f3a07bf8531e010b5d88b312273bca7
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2026-04-27 15:29:49 -0300

    deixei o navbar responsivo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import "./Navbar.css";
-import logo from "../../assets/vf.png";;
+import logo from "../../assets/vf.png";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar" id="navbar">
       <img src={logo} alt="Logo" className="logo" />
 
-      <ul className="menu">
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        ☰
+      </div>
+
+      <ul className={`menu ${open ? "active" : ""}`}>
         <li><a href="#hero">Home</a></li>
         <li><a href="#sobre">Sobre</a></li>
         <li><a href="#projects">Projetos</a></li>
